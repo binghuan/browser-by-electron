8eec0fa83993a5e4e52171485bbbcb41b8b4d955
BH_Lin <binghuanlin@gmail.com>
2020-12-07 11:09:20 +0800

    add log for events

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  console.log("-> app.on [electron-squirrel-startup]");
   app.quit();
 }
 
@@ -27,18 +28,35 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  console.log("EVENT -> app.on [ready]");
+  createWindow()
+});
+
+app.on('before-quit', () => {
+  console.log("EVENT -> app.on [before-quit]");
+});
+
+app.on('will-quit', () => {
+  console.log("EVENT -> app.on [will-quit]");
+});
+
+app.on('quit', () => {
+  console.log("EVENT -> app.on [quit]");
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
+  console.log("EVENT -> app.on [window-all-closed]");
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', () => {
+  console.log("EVENT -> app.on [activate]");
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
