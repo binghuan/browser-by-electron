8004db01e2f49e9abbc15f57d0e6d587e6c30c5f
BH_Lin <binghuanlin@gmail.com>
2020-12-07 13:52:40 +0800

    add log for events

--- src/main.js
+++ src/main.js
@@ -20,6 +20,17 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.on('close', (e) => {
+    console.log("// ------------------ EVENT -> window.on [close] ------------");
+  })
+
+  mainWindow.on('closed', (e) => {
+    console.log("// ------------------ EVENT -> window.on [closed] ------------");
+  })
+
+  mainWindow.on('ready-to-show', (e) => {
+    console.log("// ------------------ EVENT -> window.on [ready-to-show] -----");
+  })
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
@@ -29,47 +40,47 @@ const createWindow = () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-  console.log("EVENT -> app.on [ready]");
+  console.log("// ------------------ EVENT -> app.on [ready]");
   createWindow()
 });
 
 app.on('activate', () => {
-  console.log("EVENT -> app.on [activate]");
+  console.log("// ------------------ EVENT -> app.on [activate]");
 });
 
 app.on('did-become-active', () => {
-  console.log("EVENT -> app.on [did-become-active]");
+  console.log("// ------------------ EVENT -> app.on [did-become-active]");
 });
 
 
 app.on('will-finish-launching', () => {
-  console.log("EVENT -> app.on [will-finish-launching]");
+  console.log("// ------------------ EVENT -> app.on [will-finish-launching]");
 });
 
 app.on('before-quit', () => {
-  console.log("EVENT -> app.on [before-quit]");
+  console.log("// ------------------ EVENT -> app.on [before-quit]");
 });
 
 app.on('will-quit', () => {
-  console.log("EVENT -> app.on [will-quit]");
+  console.log("// ------------------ EVENT -> app.on [will-quit]");
 });
 
 app.on('quit', () => {
-  console.log("EVENT -> app.on [quit]");
+  console.log("// ------------------ EVENT -> app.on [quit]");
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  console.log("EVENT -> app.on [window-all-closed]");
+  console.log("// ------------------ EVENT -> app.on [window-all-closed]");
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', () => {
-  console.log("EVENT -> app.on [activate]");
+  console.log("// ------------------ EVENT -> app.on [activate]");
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
